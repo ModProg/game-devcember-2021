4a88cc7643406f618572fac4725944e7bc0b481c
D-H-Audio <info@dhaudio.net>
2021-12-05 17:00:09 +0100

    Updated Wwise Integration

--- wwise/GeneratedSoundBanks/wwise_ids.gd
+++ wwise/GeneratedSoundBanks/wwise_ids.gd
@@ -1,37 +1,37 @@
 class_name AK
 
 class EVENTS:
-	const MUSIC_TEST_EVENT = 1692571924
-
 	enum _enum {
-		MUSIC_TEST_EVENT = 1692571924
+	}
+
+class STATES:
+	enum _enum {
+	}
+
+class SWITCHES:
+	enum _enum {
+	}
+
+class GAME_PARAMETERS:
+	enum _enum {
+	}
+
+class TRIGGERS:
+	enum _enum {
 	}
 
 class BANKS:
-	const INIT = 1355168291
-	const MUSIC_TEST_SOUNDBANK = 3471173579
-
 	enum _enum {
-		INIT = 1355168291,
-		MUSIC_TEST_SOUNDBANK = 3471173579
 	}
 
 class BUSSES:
-	const MASTER_AUDIO_BUS = 3803692087
-
 	enum _enum {
-		MASTER_AUDIO_BUS = 3803692087
+	}
+
+class AUX_BUSSES:
+	enum _enum {
 	}
 
 class AUDIO_DEVICES:
-	const NO_OUTPUT = 2317455096
-	const SYSTEM = 3859886410
-
 	enum _enum {
-		NO_OUTPUT = 2317455096,
-		SYSTEM = 3859886410
 	}
-class AUX_BUSSES:
-	 enum _enum {}
-class GAME_PARAMETERS:
-	 enum _enum {}
