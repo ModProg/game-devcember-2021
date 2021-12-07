9060f608c76227bdc1bd8ec7b5a8269ce5476d9f
ModProg <dev@modprog.de>
2021-12-07 10:43:17 +0100

    Improved movenent

--- Testing/player.gd
+++ Testing/player.gd
@@ -3,14 +3,24 @@ extends PlatformerBody2D
 
 
 export var stop_on_slopes := true
+export var coyote_time: float = 1
 
+var jumped := false
+var time_since_ground: float
 
 func _physics_process(delta: float) -> void:
 	var hdirection := _get_hinput()
+	print(time_since_ground < coyote_time)
 	
 	apply_gravity(delta)
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		jump()
+	if Input.is_action_just_pressed("jump") and (close_to_floor() or was_on_floor()) :
+		jumped = true
+	if jumped:
+		if is_on_floor() or was_on_floor():
+			jump()
+			jumped = false
+		if !close_to_floor():
+			jumped = false
 	elif Input.is_action_just_released("jump"):
 		cut_jump()
 	
@@ -21,7 +31,17 @@ func _physics_process(delta: float) -> void:
 		apply_acceleration(delta, hdirection)
 	
 	velocity = move(delta, stop_on_slopes)
-
+	
+	if is_on_floor():
+		time_since_ground = 0
+	else:
+		time_since_ground += delta
+	
+func was_on_floor() -> bool:
+	return time_since_ground < coyote_time
+	
+func close_to_floor() -> bool:
+	return !$GroundDetector.get_overlapping_bodies().empty()
 
 func _get_hinput() -> float:
 	return sign(Input.get_action_strength("move_right") - Input.get_action_strength("move_left"))
