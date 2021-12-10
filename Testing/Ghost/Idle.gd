1512bca18593db33f5a4844467b4e940b96cc25b
Roland Fredenhagen <dev@modprog.de>
2021-12-10 17:11:46 +0100

    Ghost done

--- Testing/Ghost/Idle.gd
+++ Testing/Ghost/Idle.gd
@@ -10,7 +10,7 @@ func _on_enter(_delta) -> void:
 
 func _on_update(_delta) -> void:
 	if target.get_node("PlayerDetector").get_overlapping_bodies().empty() and !has_timer("MoveToPlayer"):
-		add_timer("MoveToPlayer", .2)
+		add_timer("MoveToPlayer", .1)
 		
 func _on_timer_timeout(name) -> void:
 	change_state(name)
