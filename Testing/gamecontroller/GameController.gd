8c713dc811ea47746f8533c95e2dc82f009e09a1
Guillermo Samaruga <guillesama1@gmail.com>
2021-12-10 20:02:18 -0300

    More Prototyping

--- Testing/gamecontroller/GameController.gd
+++ Testing/gamecontroller/GameController.gd
@@ -48,7 +48,7 @@ func load_level (scenepath) -> void:
 func _load_scene (scenepath) -> void:
 	get_tree().paused = true
 	loading_screen.show()
-	current_scene.free()
+	current_scene.queue_free()
 	var s = ResourceLoader.load(scenepath)
 	current_scene = s.instance()
 	get_tree().get_root().add_child(current_scene)
@@ -62,10 +62,59 @@ func _load_scene (scenepath) -> void:
 	get_tree().paused = false
 
 func save_checkpoint () -> void:
-	pass
+	var save_game = File.new()
+	save_game.open("user://savegame.save", File.WRITE)
+	var save_nodes = get_tree().get_nodes_in_group("Persist")
+	for node in save_nodes:
+		# Check the node is an instanced scene so it can be instanced again during load.
+		if node.filename.empty():
+			print("persistent node '%s' is not an instanced scene, skipped" % node.name)
+			continue
+
+		# Check the node has a save function.
+		if !node.has_method("save"):
+			print("persistent node '%s' is missing a save() function, skipped" % node.name)
+			continue
+
+		# Call the node's save function.
+		var node_data = node.call("save")
+
+		# Store the save dictionary as a new line in the save file.
+		save_game.store_line(to_json(node_data))
+	save_game.close()
 
 func load_checkpoint () -> void:
-	pass
+	var save_game = File.new()
+	if not save_game.file_exists("user://savegame.save"):
+		return # Error! We don't have a save to load.
+
+	# We need to revert the game state so we're not cloning objects
+	# during loading. This will vary wildly depending on the needs of a
+	# project, so take care with this step.
+	# For our example, we will accomplish this by deleting saveable objects.
+	var save_nodes = get_tree().get_nodes_in_group("Persist")
+	for i in save_nodes:
+		i.queue_free()
+
+	# Load the file line by line and process that dictionary to restore
+	# the object it represents.
+	save_game.open("user://savegame.save", File.READ)
+	while save_game.get_position() < save_game.get_len():
+		# Get the saved dictionary from the next line in the save file
+		var node_data = parse_json(save_game.get_line())
+
+		# Firstly, we need to create the object and add it to the tree and set its position.
+		var new_object = load(node_data["filename"]).instance()
+		get_node(node_data["parent"]).add_child(new_object)
+		new_object.position = Vector2(node_data["pos_x"], node_data["pos_y"])
+
+		# Now we set the remaining variables.
+		for i in node_data.keys():
+			if i == "filename" or i == "parent" or i == "pos_x" or i == "pos_y":
+				continue
+			new_object.set(i, node_data[i])
+	save_game.close()
+
 
 func pause_menu () -> void:
 	state = GameState.PAUSEMENU
@@ -78,6 +127,7 @@ func resume_paused () -> void:
 	get_tree().paused = false
 
 func quit_game () -> void:
+	get_tree().paused = true
 	state = GameState.EXITING
 	get_tree().quit()
 
--- gamecontroller/ButtonTest2.gd
+++
@@ -1,4 +0,0 @@
-extends Button
-
-func _on_Button_pressed():
-	GameController.load_level("res://gamecontroller/scene1.tscn")
